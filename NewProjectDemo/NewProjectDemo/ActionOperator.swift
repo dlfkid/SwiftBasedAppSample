5d6bf1eed6effed291ac3beac12acc9a2c2297d3
dlfkid <dlfkid@icloud.com>
2019-05-20 16:16:36 +0800

    中间层引入错误抛出机制

--- NewProjectDemo/NewProjectDemo/ActionOperator.swift
+++ NewProjectDemo/NewProjectDemo/ActionOperator.swift
@@ -25,28 +25,31 @@ class ActionOperator: NSObject {
     ///   - URL: 方法URL
     ///   - completion: 回调闭包
     /// - Returns: 返回值
-    func invokeAction(URL: URL, completion: CompletionClousure?) -> Any? {
+    func invokeAction(URL: URL, completion: CompletionClousure?) throws -> Any? {
         guard let urlString = URL.query else {
-            return nil
+            let error = ActionOperatorError.urlEmpty
+            throw error
         }
         var params = [String: Any]()
         for param in urlString.components(separatedBy: "&") {
-            let elts = param.components(separatedBy: "=")
-            if elts.count < 2 {
+            let elements = param.components(separatedBy: "=")
+            if elements.count < 2 {
                 continue
             }
-            guard let keyString = elts.first else {
+            guard let keyString = elements.first else {
                 continue
             }
-            params[keyString] = elts.last
+            params[keyString] = elements.last
         }
         
-        let actionName = URL.path.replacingOccurrences(of: "\\", with: "")
+        let actionName = URL.path.replacingOccurrences(of: "/", with: "")
         if actionName.hasPrefix("native") {
-            return false
+            let error = ActionOperatorError.invokeNativeActionFromRemote
+            throw error
         }
         
-        let result = self.invoke(targetName: URL.host ?? "", actionName: actionName, parameters: params, cache: false)
+        let result = try self.invoke(targetName: URL.host ?? "", actionName: actionName, parameters: params, cache: false)
+        
         guard let handler = completion else {
             return result
         }
@@ -62,19 +65,20 @@ class ActionOperator: NSObject {
     ///   - parameters: 参数
     ///   - cache: 是否需要缓存对象
     /// - Returns: 返回值
-    func invoke(targetName: String, actionName: String, parameters: [String: Any], cache: Bool) -> Any? {
+    func invoke(targetName: String, actionName: String, parameters: [String: Any], cache: Bool) throws ->  Any? {
         let swiftModuleName = parameters[swiftModuleNameKey] as? String
         var targetClassString = ""
         if swiftModuleName?.count ?? 0 > 0 {
             targetClassString = String("\(swiftModuleName).Target_\(targetName)")
         } else {
-            targetClassString = String("Target_\(targetName)")
+            targetClassString = String("Target\(targetName)")
         }
         var target: NSObject? = self.cachedTarget[targetClassString] as? NSObject
         
         if target == nil {
             guard let targetClass: NSObject.Type = NSClassFromString(targetClassString) as? NSObject.Type else {
-                return nil
+                let error = ActionOperatorError.targetClassNotFound(className: targetClassString)
+                throw error
             }
             target = targetClass.init()
         }
@@ -90,7 +94,8 @@ class ActionOperator: NSObject {
             return target?.perform(action)
         } else {
             self.cachedTarget.removeValue(forKey: targetClassString)
-            return nil
+            let error = ActionOperatorError.unableToInvokeFunction
+            throw error
         }
     }
     
@@ -100,5 +105,25 @@ class ActionOperator: NSObject {
     func realeaseCachedTarget(targetName: String) {
         let targetClassString = "Target_\(targetName)"
         self.cachedTarget.removeValue(forKey: targetClassString)
+    }
+}
+
+extension ActionOperator {
+    enum ActionOperatorError: Error {
+        case urlEmpty
+        case invokeNativeActionFromRemote
+        case targetClassNotFound(className: String)
+        case unableToInvokeFunction
+        
+        
+        var info: String {
+            switch self {
+            case ActionOperatorError.targetClassNotFound(let className):
+                return className
+                
+            default:
+                return ""
+            }
+        }
     }
 }
---
+++ NewProjectDemo/NewProjectDemo/Target_RemoteInvoke.swift
@@ -0,0 +1,17 @@
+
+//
+//  Target_RemoteInvoke.swift
+//  NewProjectDemo
+//
+//  Created by LeonDeng on 2019/5/20.
+//  Copyright © 2019 LeonDeng. All rights reserved.
+//
+
+import UIKit
+
+class TargetRemoteInvoke: NSObject {
+    func presentViewController(params: [String: Any]) -> UIViewController {
+        let controlelr = UIViewController()
+        return controlelr
+    }
+}
