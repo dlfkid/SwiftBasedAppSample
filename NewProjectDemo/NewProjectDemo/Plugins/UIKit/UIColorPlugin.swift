2a699caf5846f4964bdc31c33a939be375253697
dlfkid <dlfkid@icloud.com>
2019-09-06 10:20:22 +0800

    最佳预设颜色

--- NewProjectDemo/NewProjectDemo/Plugins/UIKit/UIColorPlugin.swift
+++ NewProjectDemo/NewProjectDemo/Plugins/UIKit/UIColorPlugin.swift
@@ -10,11 +10,6 @@ import UIKit.UIColor
 
 extension UIColor {
     
-    static let tint = UIColor(r: 34, g: 175, b: 197)
-    static let appLightGray = UIColor(r: 239, g: 239, b: 244)
-    static let silverGray = UIColor(r: 241, g: 239, b: 239)
-    static let tableViewBackground = UIColor(rgb: 238)
-    
     convenience init(r: CGFloat, g: CGFloat, b: CGFloat, a: CGFloat = 1.0) {
         self.init(red: r / 255.0, green: g / 255.0, blue: b / 255.0, alpha: a)
     }
@@ -39,4 +34,16 @@ extension UIColor {
                       alpha: a)
         }
     }
+}
+
+extension UIColor {
+    static let tcl_4a4a4a = UIColor(hex: "4a4a4a") // 文字主色调
+    static let tcl_999999 = UIColor(hex: "999999") // 辅助文字色调
+    static let tcl_c5c5c5 = UIColor(hex: "c5c5c5") // 置灰控件色调
+    static let tcl_d5d5d5 = UIColor(hex: "d5d5d5") // tableViewHeader文字色调
+    static let tcl_5c5558 = UIColor(hex: "535558") // 屎黄色
+    static let tcl_b88e67 = UIColor(hex: "b88e67") // 开关启动颜色
+    static let tcl_e6e6e6 = UIColor(hex: "e6e6e6") // 分割线颜色
+    static let tcl_f7f7f7 = UIColor(hex: "f7f7f7") // 背景底色，灰白
+    static let tcl_a4a4a4 = UIColor(hex: "a4a4a4") // 副标题颜色
 }
