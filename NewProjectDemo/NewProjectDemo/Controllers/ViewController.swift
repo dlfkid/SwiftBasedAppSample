3768ab2cc35792c20fdafc23339f407d317dd48a
RavenDeng <dlfkid@icloud.com>
2023-06-30 23:52:47 +0800

    feat: 接入Demo登录页

--- NewProjectDemo/NewProjectDemo/Controllers/ViewController.swift
+++ NewProjectDemo/NewProjectDemo/Controllers/ViewController.swift
@@ -10,9 +10,16 @@ import UIKit
 import PinLayout
 import Foundation
 
+struct DemoOption {
+    let title: String
+    let selectHandler: (_ option: DemoOption) -> Void
+}
+
 class ViewController: UIViewController {
     
     let demoCellIndentifier = "demoCellIdentifier"
+    
+    var dataSource = [DemoOption]()
     
     let tableview: UITableView = {
         let result = UITableView(frame: .zero, style: .plain)
@@ -23,6 +30,13 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view
+        tableview.separatorStyle = .none
+        navigationItem.title = "demo_pages"
+        dataSource = [DemoOption(title: "LoginViewController", selectHandler: { [unowned self] option in
+            print("Calling \(option.title) closure")
+            let controller = LoginViewController()
+            self.navigationController?.pushViewController(controller, animated: true)
+        })]
         tableview.delegate = self
         tableview.dataSource = self
         tableview.register(UITableViewCell.self, forCellReuseIdentifier: demoCellIndentifier)
@@ -69,16 +83,21 @@ extension ViewController: UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return dataSource.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let option = dataSource[indexPath.row]
         let cell = tableView.dequeueReusableCell(withIdentifier: demoCellIndentifier, for: indexPath)
         cell.selectionStyle = .none
+        cell.textLabel?.text = option.title
         return cell
     }
 }
 
 extension ViewController: UITableViewDelegate {
-    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let option = dataSource[indexPath.row]
+        option.selectHandler(option)
+    }
 }
